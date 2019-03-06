dadb97f0b4aba654ca95cd52185643de9574aeb7
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-03-05 22:46:01 -0700

    removed js files in dist folder

--- dist/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('ansicolor').nice;
-const log = require('ololog').configure({ locate: false });
-function _error(method, err) {
-    log.lightYellow(`${method} ERROR:`, err.message);
-}
-exports._error = _error;
-//# sourceMappingURL=error.js.map
